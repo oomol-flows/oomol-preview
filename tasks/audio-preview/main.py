5c1c194f28234a77adfa64fa80c8c12ffe93ec8a
TaoZeyu <i@taozeyu.com>
2024-12-16 12:04:22 +0800

    feat(blocks): implements by context.preview

--- tasks/audio-preview/main.py
+++ tasks/audio-preview/main.py
@@ -2,6 +2,6 @@ from oocana import Context, MediaPreviewPayload
 
 def main(inputs: dict, context: Context):
   context.preview(MediaPreviewPayload(
-    type="markdown",
-    data=inputs["text"],
+    type="audio",
+    data=inputs["audio_path"],
   ))
--- tasks/video-preview/main.py
+++ tasks/video-preview/main.py
@@ -1,2 +1,7 @@
-def main():
-  pass
+from oocana import Context, MediaPreviewPayload
+
+def main(inputs: dict, context: Context):
+  context.preview(MediaPreviewPayload(
+    type="video",
+    data=inputs["video_path"],
+  ))
