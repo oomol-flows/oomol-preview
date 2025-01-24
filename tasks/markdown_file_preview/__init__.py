7ac29be4719b7ade20bd47fde4de5c6bd6fdb922
shaun <beingswu@gmail.com>
2025-01-24 11:29:41 +0800

    update code style

--- tasks/markdown_file_preview/__init__.py
+++ tasks/markdown_file_preview/__init__.py
@@ -3,7 +3,6 @@ from oocana import Context, MediaPreviewPayload
 
 def main(params: dict, context: Context):
     file_path = params.get("file_path")
-    # your code
     markdown_content = _read_markdown_file(file_path)
 
     if markdown_content is not None:
@@ -17,15 +16,13 @@ def main(params: dict, context: Context):
     return {"text": markdown_content}
 
 
-# 读取 .md 文件
 def _read_markdown_file(file_path):
     try:
         with open(file_path, "r", encoding="utf-8") as file:
             markdown_text = file.read()
         return markdown_text
-    except FileNotFoundError:
-        print(f"文件 {file_path} 未找到")
-        return None
+    except FileNotFoundError as e:
+        print(f"File {file_path} not found.")
     except Exception as e:
-        print(f"读取文件时发生错误: {e}")
-        return None
+        print(f"An error occurred while reading the file: {e}")
+        raise e
