adc8375df0ca746f05e3701a16078c2ecced83e6
Moskize91 <i@taozeyu.com>
2025-06-09 15:28:34 +0800

    chore(project): fix install wrong dependencies & upgrade dependencies (#8)

--- flows/preview-image/scriptlets/+typescript#1.ts
+++ flows/preview-image/scriptlets/+typescript#1.ts
@@ -3,10 +3,10 @@ import type { Context } from "@oomol/types/oocana";
 //#region generated meta
 type Inputs = {
   file: string;
-}
+};
 type Outputs = {
   files: string[];
-}
+};
 //#endregion
 
 export default async function({ file }: Inputs): Promise<Outputs> {
